acfdfdc8bb222ffe0b00b33df77ee8ad0bdcdb49
lipegomes <fgdl.py91@gmail.com>
2021-05-13 19:56:34 -0300

    Update

---
+++ src/main/calculator.py
@@ -0,0 +1,18 @@
+def sum(x, y):
+    return f"Result of the sum between {x} and {y} is: {x + y}"
+
+
+def subtraction(x, y):
+    return f"Result of the subtraction between {x} and {y} is: {x - y}"
+
+
+def multiplication(x, y):
+    return f"Result of the multiplication between {x} and {y} is: {x * y}"
+
+
+def division(x, y):
+    return f"Result of the division between {x} and {y} is: {x / y}"
+
+
+def exponentiation(x, y):
+    return f"Result of the exponentiation between {x} and {y} is: {x ** y}"
---
+++ src/tests/test_calculadora.py
@@ -0,0 +1,15 @@
+from src.main.calculator import *
+calc1 = sum(10, 30)
+print(calc1)
+
+calc2 = subtraction(25, 15)
+print(calc2)
+
+calc3 = multiplication(7, 8.5)
+print(calc3)
+
+calc4 = division(2048, 64)
+print(calc4)
+
+calc5 = exponentiation(7, 7)
+print(calc5)
