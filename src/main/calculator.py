579838958f8ea95f01966f88189f124548d7abc3
lipegomes <fgdl.py91@gmail.com>
2021-05-17 12:33:34 -0300

    Added doctest

--- src/main/calculator.py
+++ src/main/calculator.py
@@ -1,28 +1,73 @@
 def sum(x, y):
+    """
+    Sum x and y
+
+    >>> sum(11, 11)
+    22
+
+    >>> sum(10, 20)
+    30
+
+    """
     assert isinstance(x, (int, float)), "The x value must be an int or float"
     assert isinstance(y, (int, float)), "The y value must be an int or float"
-    return f"Result of the sum between {x} and {y} is: {x + y}"
+    return x + y
 
 
 def subtraction(x, y):
+    """
+    Subtraction x and y
+
+    >>> subtraction(-20, 80)
+    -100
+    """
     assert isinstance(x, (int, float)), "The x value must be an int or float"
     assert isinstance(y, (int, float)), "The y value must be an int or float"
-    return f"Result of the subtraction between {x} and {y} is: {x - y}"
+    return x - y
 
 
 def multiplication(x, y):
+    """
+    Multiplication x and y
+
+    >>> multiplication('25', 10)
+    Traceback (most recent call last):
+    ...
+    AssertionError: The x value must be an int or float
+
+     >>> multiplication(5, 10)
+     50
+    """
     assert isinstance(x, (int, float)), "The x value must be an int or float"
     assert isinstance(y, (int, float)), "The y value must be an int or float"
-    return f"Result of the multiplication between {x} and {y} is: {x * y}"
+    return x * y
 
 
 def division(x, y):
+    """
+    Division x and y
+
+    >>> division(2048, 64)
+    32.0
+
+    """
     assert isinstance(x, (int, float)), "The numerator value must be an int or float"
     assert isinstance(y, (int, float)), "The denominator value must be greater than zero"
-    return f"Result of the division between {x} and {y} is: {x / y}"
+    return x / y
 
 
 def exponentiation(x, y):
+    """
+    Exponentiation x and y
+
+    >>> exponentiation(2, -3)
+    0.125
+    """
     assert isinstance(x, (int, float)), "The x value must be an int or float"
     assert isinstance(y, (int, float)), "The y value must be an int or float"
-    return f"Result of the exponentiation between {x} and {y} is: {x ** y}"
+    return x ** y
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
